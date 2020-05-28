90d89ce55ab4e3a79e3945ddf118be6458f3c0ad
sogna <win778500@gmail.com>
2020-05-29 01:23:52 +0900

    v1

--- code/v1/transform.py
+++ code/v1/transform.py
@@ -31,6 +31,33 @@ def transform_v0(config):
     return train_transform, test_transform
 
 
+def transform_v1(config):
+    train_transform = Compose([
+        VerticalFlip(p=0.5),
+        HorizontalFlip(p=0.5),
+        Cutout(num_holes=4, max_h_size=4, max_w_size=4, p=0.5),
+        Cutout(num_holes=8, max_h_size=8, max_w_size=8, p=0.5),
+        Cutout(num_holes=8, max_h_size=16, max_w_size=16, p=0.5),
+        Cutout(num_holes=16, max_h_size=8, max_w_size=8, p=0.5),
+        Cutout(num_holes=32, max_h_size=8, max_w_size=8, p=0.5),
+        ToTensor(),
+        Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225],
+        ),
+    ], p=1)
+
+    test_transform = Compose([
+        ToTensor(),
+        Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225],
+        )
+    ], p=1)
+
+    return train_transform, test_transform
+
+
 def get_transform(config):
     try:
         name = f"transform_v{config.transform_version}"
