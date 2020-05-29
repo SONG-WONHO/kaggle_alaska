1bf04220970d0f658b12efc978f641493fa6f363
sogna <win778500@gmail.com>
2020-05-29 17:45:00 +0900

    Update transform.py

--- code/v1/transform.py
+++ code/v1/transform.py
@@ -1,7 +1,7 @@
 from albumentations.pytorch import ToTensor
 from albumentations import (
     Compose, HorizontalFlip, VerticalFlip, Normalize, Cutout, PadIfNeeded, RandomCrop, ToFloat,
-    RandomGridShuffle
+    RandomGridShuffle, ChannelShuffle
 )
 import cv2
 
@@ -139,6 +139,39 @@ def transform_v3(config):
             std=[0.229, 0.224, 0.225],
         ),
         ToTensor(),
+    ], p=1)
+
+    return train_transform, test_transform
+
+
+def transform_v4(config):
+    """ ChannelShuffle, VFlip, Hflip, Cutout, Normalize
+
+    :param config: CFG
+    :return: (train transform, test transform)
+    """
+    train_transform = Compose([
+        ChannelShuffle(p=0.5),
+        VerticalFlip(p=0.5),
+        HorizontalFlip(p=0.5),
+        Cutout(num_holes=4, max_h_size=4, max_w_size=4, p=0.5),
+        Cutout(num_holes=8, max_h_size=8, max_w_size=8, p=0.5),
+        Cutout(num_holes=8, max_h_size=16, max_w_size=16, p=0.5),
+        Cutout(num_holes=16, max_h_size=8, max_w_size=8, p=0.5),
+        Cutout(num_holes=32, max_h_size=8, max_w_size=8, p=0.5),
+        Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225],
+        ),
+        ToTensor()
+    ], p=1)
+
+    test_transform = Compose([
+        Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225],
+        ),
+        ToTensor()
     ], p=1)
 
     return train_transform, test_transform
