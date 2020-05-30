c89cd2f385aa234a4caa6bee6a23c143a3622eb3
sogna <win778500@gmail.com>
2020-05-30 14:19:36 +0900

    Update transform.py

--- code/v1/transform.py
+++ code/v1/transform.py
@@ -210,6 +210,52 @@ def transform_v5(config):
     return train_transform, test_transform
 
 
+def transform_v6(config):
+    """ GridShuffle, GridDropout, VFlip, Hflip, Cutout, Normalize
+
+    :param config: CFG
+    :return: (train transform, test transform)
+    """
+
+    grid_shuffle = OneOf([
+        RandomGridShuffle((2, 2)),
+        RandomGridShuffle((4, 4)),
+        RandomGridShuffle((6, 6)),
+    ])
+
+    grid_dropout = OneOf([
+        GridDropout(holes_number_x=6, holes_number_y=6, random_offset=True),
+        GridDropout(holes_number_x=8, holes_number_y=8, random_offset=True),
+        GridDropout(holes_number_x=10, holes_number_y=10, random_offset=True),
+    ])
+
+    train_transform = Compose([
+        VerticalFlip(p=0.5),
+        HorizontalFlip(p=0.5),
+        OneOf([grid_shuffle, grid_dropout], p=0.66),
+        Cutout(num_holes=4, max_h_size=4, max_w_size=4, p=0.5),
+        Cutout(num_holes=8, max_h_size=8, max_w_size=8, p=0.5),
+        Cutout(num_holes=8, max_h_size=16, max_w_size=16, p=0.5),
+        Cutout(num_holes=16, max_h_size=8, max_w_size=8, p=0.5),
+        Cutout(num_holes=32, max_h_size=8, max_w_size=8, p=0.5),
+        Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225],
+        ),
+        ToTensor()
+    ], p=1)
+
+    test_transform = Compose([
+        Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225],
+        ),
+        ToTensor()
+    ], p=1)
+
+    return train_transform, test_transform
+
+
 def get_transform(config):
     try:
         name = f"transform_v{config.transform_version}"
