b22378f5b6190406dbb4dd49f5cce09c278564f7
sogna <win778500@gmail.com>
2020-06-06 02:11:43 +0900

    transform v8

--- code/v6/transform.py
+++ code/v6/transform.py
@@ -298,6 +298,56 @@ def transform_v7(config):
     return train_transform, test_transform
 
 
+def transform_v8(config):
+    """ GridShuffle * n, GridDropout * n, VFlip, Hflip, Cutout, Normalize
+
+    :param config: CFG
+    :return: (train transform, test transform)
+    """
+
+    grid_shuffle = OneOf([
+        RandomGridShuffle((2, 2)),
+        RandomGridShuffle((4, 4)),
+        RandomGridShuffle((8, 8)),
+        RandomGridShuffle((16, 16)),
+        RandomGridShuffle((32, 32)),
+    ])
+
+    grid_dropout = OneOf([
+        GridDropout(holes_number_x=2, holes_number_y=2, random_offset=True),
+        GridDropout(holes_number_x=4, holes_number_y=4, random_offset=True),
+        GridDropout(holes_number_x=8, holes_number_y=8, random_offset=True),
+        GridDropout(holes_number_x=16, holes_number_y=16, random_offset=True),
+        GridDropout(holes_number_x=32, holes_number_y=32, random_offset=True),
+    ])
+
+    train_transform = Compose([
+        VerticalFlip(p=0.5),
+        HorizontalFlip(p=0.5),
+        OneOf([grid_shuffle, grid_dropout], p=0.66),
+        Cutout(num_holes=4, max_h_size=4, max_w_size=4, p=0.5),
+        Cutout(num_holes=8, max_h_size=8, max_w_size=8, p=0.5),
+        Cutout(num_holes=8, max_h_size=16, max_w_size=16, p=0.5),
+        Cutout(num_holes=16, max_h_size=8, max_w_size=8, p=0.5),
+        Cutout(num_holes=32, max_h_size=8, max_w_size=8, p=0.5),
+        Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225],
+        ),
+        ToTensor()
+    ], p=1)
+
+    test_transform = Compose([
+        Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225],
+        ),
+        ToTensor()
+    ], p=1)
+
+    return train_transform, test_transform
+
+
 def get_transform(config):
     try:
         name = f"transform_v{config.transform_version}"
