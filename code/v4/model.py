8037e3fcddfd188f8b8db019c8c97c89a7be26e2
sogna <win778500@gmail.com>
2020-06-09 09:08:41 +0900

    Update model.py

--- code/v4/model.py
+++ code/v4/model.py
@@ -49,6 +49,47 @@ class BaseModel(nn.Module):
         return outputs
 
 
+class GridModel(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.gs = 16
+        self.config = config
+
+        self.model = EfficientNet.from_pretrained('efficientnet-b0')
+
+        self.pool_layer = LinearPool()
+
+        def get_reg_layer():
+            return nn.Sequential(
+                nn.Dropout(0.15),
+                nn.Linear(1280, config.num_targets),
+            )
+
+        self.dense_out = get_reg_layer()
+
+    def forward(self, x):
+        bs, c, w, h = x.size()
+        gs = self.gs
+        w_t, h_t = 16, 16
+
+        x = x.reshape(bs, c, gs, int(w//gs), gs, int(h//gs))
+        x = x.permute(0, 1, 2, 4, 3, 5)
+        x = x.reshape(bs, c, gs * gs, int(w//gs), int(h//gs))
+        x = x.permute(0, 2, 1, 3, 4)
+        x = x.reshape(bs * gs * gs, c, int(w//gs), int(h//gs))
+
+        feat = self.model.extract_features(x)
+        feat = feat.reshape(bs, gs * gs, 1280, int(w_t//gs), int(h_t//gs))
+        feat = feat.permute(0, 2, 1, 3, 4)
+        feat = feat.reshape(bs, 1280, gs, gs, int(w_t//gs), int(h_t//gs))
+        feat = feat.permute(0, 1, 2, 4, 3, 5)
+        feat = feat.reshape(bs, 1280, gs * int(w_t//gs), gs * int(h_t//gs))
+
+        feat = self.pool_layer(feat)
+        outputs = self.dense_out(feat)
+        return outputs
+
+
 def get_model(config):
     try:
         f = globals().get(f"{config.model_name}")
