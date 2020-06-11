ad0e45a4cf5118379b98018a97ccbaf122e31a56
sogna <win778500@gmail.com>
2020-06-12 01:22:47 +0900

    Update learner.py

--- code/v6/learner.py
+++ code/v6/learner.py
@@ -167,6 +167,8 @@ class Learner(object):
 
             preds = model(X_batch)
 
+            print(preds.shape, y_batch.shape)
+
             loss = loss_func(preds, y_batch.view(-1))
             losses.update(loss.item(), batch_size)
 
